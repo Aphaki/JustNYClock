3cd47f399ec492fed0caac84afbdd7b12cc25be9
Aphaki <kiapha1108@gmail.com>
2022-04-26 18:22:21 +0900

    그냥 실시간 뉴욕시계
    Timer.publish 활용함
    아무 로직 없음 그저 시계
    그래서 모델은 없음

--- JustNYClock/ContentView.swift
+++ JustNYClock/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    var timer = Timer.publish(every: 1.0, on: .main, in: .default).autoconnect()
+    
+    let formatter: DateFormatter = {
+        let df = DateFormatter()
+        df.timeZone = TimeZone(identifier: "America/New_York")
+        df.locale = Locale(identifier: "ko_KR")
+        df.dateStyle = .medium
+        df.timeStyle = .medium
+        return df
+    }()
+    @State var dateStr = "Loading..."
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(colors: [.pink, .white], startPoint: .leading, endPoint: .trailing)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                Text("New_York Clock")
+                    .fontWeight(.black)
+                Text(dateStr)
+                    .font(.title)
+                    .fontWeight(.thin)
+                    .onReceive(timer) { output in
+                       let dateString = formatter.string(from: output)
+                        self.dateStr = dateString
+                }
+            } //VStack
+        } //ZStack
     }
 }
 
