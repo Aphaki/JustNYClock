4fb46ebfa82e3656919243ddad8165cd72343b8d
Aphaki <kiapha1108@gmail.com>
2022-04-26 18:15:18 +0900

    Initial Commit

---
+++ JustNYClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustNYClock
+//
+//  Created by Sy Lee on 2022/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JustNYClock/JustNYClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNYClockApp.swift
+//  JustNYClock
+//
+//  Created by Sy Lee on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct JustNYClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
